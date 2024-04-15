4e96d587e5558c7607fdc2d52e066b6b3008a866
Corey <corey.a.tess@gmail.com>
2024-04-15 18:04:55 -0400

    Update Stepper and Select
    Select for character options (Bloodline, Origin, Post) is now a list, with a detail view, not a dropdown.
    Stepper is vertical, and displays selection

--- app/src/lib/components/character/create-character-form.svelte
+++ app/src/lib/components/character/create-character-form.svelte
@@ -11,13 +11,16 @@
   import SuperDebug, { superForm } from 'sveltekit-superforms';
   import { zod } from 'sveltekit-superforms/adapters';
   import * as RadioGroup from '$lib/components/ui/radio-group';
-  import * as Select from '$lib/components/ui/select';
   import Label from '$lib/components/ui/label/label.svelte';
+  import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
   import { page } from '$app/stores';
   import { cn } from '$lib/utils';
   import Icon from '@iconify/svelte';
-  import { string } from 'zod';
-  import { getCharacterOption } from '$lib/utils/sanity';
+  import { slide } from 'svelte/transition';
+  import { quintOut } from 'svelte/easing';
+  import type { Bloodline } from '$lib/types/sanity.types';
+  import { type Writable, writable } from 'svelte/store';
+  import CharacterOptionSelect from '../character-option-select.svelte';
 
   export let bloodlines: string[];
   export let origins: string[];
@@ -26,15 +29,27 @@
   export let data;
 
   const steps = [
-    zod(characterSchemaStep1),
-    zod(characterSchemaStep2),
-    zod(characterSchemaStep3),
-    zod(characterSchemaStep4)
+    {
+      stepName: 'Name',
+      validator: zod(characterSchemaStep1)
+    },
+    {
+      stepName: 'Bloodline',
+      validator: zod(characterSchemaStep2)
+    },
+    {
+      stepName: 'Origin',
+      validator: zod(characterSchemaStep3)
+    },
+    {
+      stepName: 'Post',
+      validator: zod(characterSchemaStep4)
+    }
   ];
 
   let step = 1;
 
-  $: options.validators = steps[step - 1];
+  $: options.validators = steps[step - 1].validator;
 
   const form = superForm(data, {
     dataType: 'json',
@@ -62,7 +77,9 @@
 
   $: disabled = initialStepData == $formData || !!$allErrors.length;
 
-  let selectedBloodline;
+  let selectedBloodline: Writable<Bloodline> = writable({} as Bloodline);
+  let selectedOrigin: Writable<Bloodline> = writable({} as Bloodline);
+  let selectedPost: Writable<Bloodline> = writable({} as Bloodline);
 </script>
 
 {#if $message}
@@ -71,137 +88,116 @@
   </div>
 {/if}
 
-<form method="post" use:enhance>
-  <div class="mt-[15px] flex border-t py-4">
-    <div class="-mt-[37px] flex w-full flex-row items-center justify-between gap-4">
+<div class="flex h-full flex-row gap-2">
+  <div class="flex justify-center py-4 align-middle">
+    <!-- <Separator class="" orientation="vertical" /> -->
+    <div class="flex w-full flex-col items-center justify-between gap-4">
       {#each steps as _, i}
         <div class={cn(i + 1 === step ? 'flex-1' : '', 'transition-all duration-500')}>
-          <Badge class={cn(i + 1 === step ? 'bg-purple-800' : '')}>
-            {i + 1}
+          <Badge class={cn(i + 1 === step ? 'bg-purple-500' : '', 'w-28 rounded-sm')}>
+            <div class="flex flex-col gap-1">
+              <div class="text-base">{_.stepName}</div>
+              <div class="text-sm">{$formData[_.stepName.toLocaleLowerCase()]}</div>
+            </div>
           </Badge>
         </div>
       {/each}
     </div>
   </div>
 
-  {#if step === 1}
-    <Form.Field {form} name="name">
-      <Form.Control let:attrs>
-        <Form.Label>Name:</Form.Label>
-        <Input {...attrs} bind:value={$formData.name} />
-      </Form.Control>
-      <Form.Description>The name for this character</Form.Description>
-      <Form.FieldErrors />
-    </Form.Field>
-    <Form.Field {form} name="characterType">
-      <Form.Control let:attrs>
-        <RadioGroup.Root bind:value={$formData.characterType}>
-          <div class="flex items-center space-x-2">
-            <RadioGroup.Item value="freeform" id="freeform" />
-            <Label for="freeform">Freeform Character</Label>
-          </div>
-          <div class="flex items-center space-x-2">
-            <RadioGroup.Item value="quickStart" id="quickStart" />
-            <Label for="quickStart">Quickstart Character</Label>
-          </div>
-        </RadioGroup.Root>
-      </Form.Control>
-      <Form.Description>Choose the type of character you want to create</Form.Description>
-      <Form.FieldErrors />
-    </Form.Field>
-  {:else if step === 2}
-    <Form.Field {form} name="bloodline">
-      <Form.Control let:attrs>
-        <Form.Label>Bloodline:</Form.Label>
-        <Select.Root
-          selected={$formData.bloodline}
-          onSelectedChange={(v) => {
-            v && ($formData.bloodline = v.value);
+  <form class="w-full flex flex-col" method="post" use:enhance>
+    {#if step === 1}
+      <div
+        class="h-full"
+        transition:slide={{ delay: 100, duration: 500, easing: quintOut, axis: 'y' }}
+      >
+        <Form.Field {form} name="name">
+          <Form.Control let:attrs>
+            <Form.Label>Name:</Form.Label>
+            <Input {...attrs} bind:value={$formData.name} />
+          </Form.Control>
+          <Form.Description>The name for this character</Form.Description>
+          <Form.FieldErrors />
+        </Form.Field>
+        <Form.Field {form} name="characterType">
+          <Form.Control let:attrs>
+            <RadioGroup.Root bind:value={$formData.characterType}>
+              <div class="flex items-center space-x-2">
+                <RadioGroup.Item value="freeform" id="freeform" />
+                <Label for="freeform">Freeform Character</Label>
+              </div>
+              <div class="flex items-center space-x-2">
+                <RadioGroup.Item value="quickStart" id="quickStart" />
+                <Label for="quickStart">Quickstart Character</Label>
+              </div>
+            </RadioGroup.Root>
+          </Form.Control>
+          <Form.Description>Choose the type of character you want to create</Form.Description>
+          <Form.FieldErrors />
+        </Form.Field>
+      </div>
+    {:else if step === 2}
+      <div
+        class="h-full"
+        transition:slide={{ delay: 100, duration: 1500, easing: quintOut, axis: 'y' }}
+      >
+        <CharacterOptionSelect
+          {form}
+          options={bloodlines}
+          optionType="bloodline"
+          on:select={(e) => {
+            $formData['bloodline'] = e.detail;
           }}
-        >
-          <Select.Trigger {...attrs} class="w-44">
-            <Select.Value placeholder="Select a Bloodline" />
-          </Select.Trigger>
-          <Select.Content>
-            {#each bloodlines as bloodline}
-              <Select.Item value={bloodline} />
-            {/each}
-          </Select.Content>
-        </Select.Root>
-      </Form.Control>
-      <Form.Description>
-        {selectedBloodline}
-      </Form.Description>
-      <Form.FieldErrors />
-    </Form.Field>
-  {:else if step === 3}
-    <Form.Field {form} name="origin">
-      <Form.Control let:attrs>
-        <Form.Label>Origin:</Form.Label>
-        <Select.Root
-          selected={$formData.origin}
-          onSelectedChange={(v) => {
-            v && ($formData.origin = v.value);
+        />
+      </div>
+    {:else if step === 3}
+      <div
+        class="h-full"
+        transition:slide={{ delay: 100, duration: 1500, easing: quintOut, axis: 'y' }}
+      >
+        <CharacterOptionSelect
+          {form}
+          options={origins}
+          optionType="origin"
+          on:select={(e) => {
+            $formData['origin'] = e.detail;
           }}
-        >
-          <Select.Trigger {...attrs} class="w-44">
-            <Select.Value placeholder="Select an Origin" />
-          </Select.Trigger>
-          <Select.Content>
-            {#each origins as origin}
-              <Select.Item value={origin} />
-            {/each}
-          </Select.Content>
-        </Select.Root>
-      </Form.Control>
-      <Form.Description>The Origin of this character</Form.Description>
-      <Form.FieldErrors />
-    </Form.Field>
-  {:else if step === 4}
-    <Form.Field {form} name="post">
-      <Form.Control let:attrs>
-        <Form.Label>Post:</Form.Label>
-        <Select.Root
-          selected={$formData.post}
-          onSelectedChange={(v) => {
-            v && ($formData.post = v.value);
+        />
+      </div>
+    {:else if step === 4}
+      <div
+        class="h-full"
+        transition:slide={{ delay: 100, duration: 1500, easing: quintOut, axis: 'y' }}
+      >
+        <CharacterOptionSelect
+          {form}
+          options={posts}
+          optionType="post"
+          on:select={(e) => {
+            $formData['post'] = e.detail;
           }}
-        >
-          <Select.Trigger {...attrs} class="w-44">
-            <Select.Value placeholder="Select a Post" />
-          </Select.Trigger>
-          <Select.Content>
-            {#each posts as post}
-              <Select.Item value={post} />
-            {/each}
-          </Select.Content>
-        </Select.Root>
-      </Form.Control>
-      <Form.Description>The Post of this character</Form.Description>
-      <Form.FieldErrors />
-    </Form.Field>
-  {/if}
-
-  <div class="flex justify-between py-4">
-    {#if step === steps.length}
-      <Form.Button class="text-base space-x-1" on:click={ () => step-- }>
-        <Icon icon="ph:arrow-left-light" class="h-5 w-5" />
-        Back
-      </Form.Button>
-      <Form.Button class="text-base space-x-1 bg-green-500" disabled={disabled}>
-        Create
-      </Form.Button>
-    {:else}
-      <Form.Button class="text-base space-x-1" disabled={step === 1} on:click={ () => step-- }>
-        <Icon icon="ph:arrow-left-light" class="h-5 w-5" />
-        Back
-      </Form.Button>
-      <Form.Button class="text-base space-x-1" disabled={disabled}>
-        Next
-        <Icon icon="ph:arrow-right-light" class="h-5 w-5" />
-      </Form.Button>
+        />
+      </div>
     {/if}
-  </div>
 
-  <SuperDebug data={$formData} />
-</form>
+    <div class="flex justify-between py-4">
+      {#if step === steps.length}
+        <Form.Button class="space-x-1 text-base" on:click={() => step--}>
+          <Icon icon="ph:arrow-left-light" class="h-5 w-5" />
+          Back
+        </Form.Button>
+        <Form.Button class="space-x-1 bg-green-500 text-base" {disabled}>Create</Form.Button>
+      {:else}
+        <Form.Button class="space-x-1 text-base" disabled={step === 1} on:click={() => step--}>
+          <Icon icon="ph:arrow-left-light" class="h-5 w-5" />
+          Back
+        </Form.Button>
+        <Form.Button class="space-x-1 text-base" {disabled}>
+          Next
+          <Icon icon="ph:arrow-right-light" class="h-5 w-5" />
+        </Form.Button>
+      {/if}
+    </div>
+    <SuperDebug data={$formData} />
+  </form>
+</div>
